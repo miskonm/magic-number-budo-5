d9deb7c83b57629813429f7ce4ac5cba384f0f79
Nikita Misko <miskonm@gmail.com>
2023-06-22 21:44:45 +0300

    Add magic numbers logic

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -2,13 +2,56 @@ using UnityEngine;
 
 public class MagicNumbers : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Variables
+
+    public int Max = 1000;
+    public int Min = 1;
+    private int _guess;
+
+    #endregion
+
+    #region Unity lifecycle
+
     private void Start()
     {
+        Debug.Log($"Привет! Загадай число от {Min} до {Max}");
+        CalculateGuess();
+        AskAboutGuess();
     }
 
-    // Update is called once per frame
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Max = _guess;
+            CalculateGuess();
+            AskAboutGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Min = _guess;
+            CalculateGuess();
+            AskAboutGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log($"Поздравляю! Я угадал! Твое число {_guess}");
+        }
     }
+
+    #endregion
+
+    #region Private methods
+
+    private void AskAboutGuess()
+    {
+        Debug.Log($"Твое число {_guess}?");
+    }
+
+    private void CalculateGuess()
+    {
+        _guess = (Min + Max) / 2;
+    }
+
+    #endregion
 }
